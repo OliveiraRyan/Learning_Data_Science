877417594f1f14865d7ebd4be6e6fe1bc7b29cd1
Ryan Oliveira <olives@pop-os.localdomain>
2021-09-30 01:37:41 -0400

    Added queries+data for vaccs

--- Alex_Portfolio_Project_1/Tableau_Queries.sql
+++ Alex_Portfolio_Project_1/Tableau_Queries.sql
@@ -4,9 +4,25 @@ Queries for Tableau Project
 
 */
 
+/*
+ ---------------------------------------------
+DATA CLEANING
+ ---------------------------------------------
+ */
+
 show databases;
 use PortfolioProject;
 
+-- Update Population of Northern Cyprus to 326000
+UPDATE PortfolioProject.CovidDeaths
+SET population = 326000
+WHERE location = 'Northern Cyprus'
+
+/*
+ ---------------------------------------------
+ DASHBOARD 1 - Covid Deaths && Infection %
+ ---------------------------------------------
+ */
 
 -- 1.
 
@@ -27,7 +43,7 @@ ORDER BY 1,2
 
 -- 2.
 
--- Looking at the total death count per country, organized in descending order
+-- Looking at the total death count per continent, organized in descending order
 SELECT location, SUM(new_deaths) AS total_death_count
 FROM PortfolioProject.CovidDeaths cd 
 WHERE continent = ''
@@ -51,7 +67,7 @@ ORDER BY percent_population_infected DESC
 -- 4. 
 
 -- Looking at each Country's Infection Rate compared to Population
--- Across each day, in descending order
+-- Across each day, in descending order (of percent_population_infected)
 SELECT location, population, `date`, MAX(total_cases) AS highest_infection_count
 , COALESCE(MAX(total_cases/population), 0) AS percent_population_infected
 FROM PortfolioProject.CovidDeaths cd 
@@ -65,11 +81,145 @@ ORDER BY percent_population_infected DESC -- , highest_infection_count, `date`
 
 
 
+/*
+ ---------------------------------------------
+ DASHBOARD 2 - Covid Vaccs && Vaccination %
+ ---------------------------------------------
+ */
 
 
+SELECT *
+FROM CovidDeaths cd
+-- WHERE cd.location LIKE '%world%'
+WHERE location = 'Northern Cyprus'
+
+SELECT *
+FROM CovidVaccinations cv 
+-- WHERE cd.location LIKE '%world%'
+WHERE location = 'Asia'
 
 
--- Queries I would like to add into Tableau at a later time
+-- 5.
+
+-- Looking at Worldwide vaccination percentages for partial and complete vaccinations
+SELECT SUM(population) as total_population
+, SUM(people_vaccinated) as total_people_vaccinated
+, SUM(people_fully_vaccinated) as total_people_fully_vaccinated
+, SUM(people_vaccinated) / SUM(population) as percentage_population_vaccinated
+, SUM(people_fully_vaccinated) / SUM(population) as percentage_population_fully_vaccinated
+FROM (SELECT cd.location, MAX(cd.population) as population
+	, MAX(cv.people_vaccinated) as people_vaccinated
+	, MAX(cv.people_fully_vaccinated) as people_fully_vaccinated
+	FROM PortfolioProject.CovidDeaths cd 
+	JOIN PortfolioProject.CovidVaccinations cv
+		ON cd.location = cv.location 
+		AND cd.`date` = cv.`date` 
+	WHERE cd.continent != ''
+	GROUP BY cd.location
+) VaccinationsByLocation
+
+
+-- Double checking based on data provided under location 'World'
+-- Numbers are very close, so we'll keep them
+
+SELECT cd.population, cv.people_vaccinated, cv.people_fully_vaccinated
+, cv.people_vaccinated / cd.population as percentage_population_vaccinated
+, cv.people_fully_vaccinated / cd.population as percentage_population_fully_vaccinated
+FROM PortfolioProject.CovidDeaths cd 
+JOIN PortfolioProject.CovidVaccinations cv
+	ON cd.location = cv.location 
+	AND cd.`date` = cv.`date` 
+WHERE cd.location LIKE '%world%'
+ORDER BY cd.`date` DESC
+LIMIT 1
+
+
+-- 6.
+
+-- Looking at total vaccination count by continent, organized in descending order
+SELECT cd.location
+, MAX(cv.people_vaccinated) as people_vaccinated
+, MAX(cv.people_fully_vaccinated) as people_fully_vaccinated
+FROM PortfolioProject.CovidDeaths cd 
+JOIN PortfolioProject.CovidVaccinations cv
+	ON cd.location = cv.location 
+	AND cd.`date` = cv.`date` 
+WHERE cd.continent = ''
+AND cd.location NOT IN ('World', 'European Union', 'International')
+GROUP BY cd.location
+ORDER BY people_vaccinated DESC
+
+
+-- 7.
+
+-- Looking at each Country's Vaccination Rate compared to Population
+-- As of recent date, in descending order (of people_vaccinated)
+SELECT cd.location, cd.population
+, MAX(cv.people_vaccinated) as people_vaccinated
+, MAX(cv.people_fully_vaccinated) as people_fully_vaccinated
+, CASE 
+	WHEN MAX(cv.people_vaccinated) / MAX(cd.population) > 0.99 THEN '>0.99'
+	ELSE MAX(cv.people_vaccinated) / MAX(cd.population)
+END as percentage_vaccinated
+, CASE 
+	WHEN MAX(cv.people_fully_vaccinated) / MAX(cd.population) > 0.99 THEN '>0.99'
+	ELSE MAX(cv.people_fully_vaccinated) / MAX(cd.population)
+END as percentage_fully_vaccinated
+FROM PortfolioProject.CovidDeaths cd 
+JOIN PortfolioProject.CovidVaccinations cv
+	ON cd.location = cv.location 
+	AND cd.`date` = cv.`date` 
+WHERE cd.continent != ''
+GROUP BY cd.location, cd.population 
+ORDER BY 
+	CASE REGEXP_LIKE(percentage_vaccinated, '^0.[0-9.]+$')
+		WHEN 1 THEN percentage_vaccinated
+		ELSE 5
+	END DESC
+
+
+-- 8.
+
+-- Looking at each Country's Vaccination Rate compared to Population
+-- Across each day, in descending order (of percentage_vaccinated)
+SELECT cd.location, cd.population, cd.`date` 
+, MAX(cv.people_vaccinated) as people_vaccinated
+, MAX(cv.people_fully_vaccinated) as people_fully_vaccinated
+, CASE 
+	WHEN MAX(cv.people_vaccinated) / MAX(cd.population) > 0.99 THEN '>0.99'
+	ELSE MAX(cv.people_vaccinated) / MAX(cd.population)
+END as percentage_vaccinated
+, CASE 
+	WHEN MAX(cv.people_fully_vaccinated) / MAX(cd.population) > 0.99 THEN '>0.99'
+	ELSE MAX(cv.people_fully_vaccinated) / MAX(cd.population)
+END as percentage_fully_vaccinated
+FROM PortfolioProject.CovidDeaths cd 
+JOIN PortfolioProject.CovidVaccinations cv
+	ON cd.location = cv.location 
+	AND cd.`date` = cv.`date` 
+WHERE cd.continent != ''
+GROUP BY cd.location, cd.population, cd.`date` 
+ORDER BY 
+	CASE REGEXP_LIKE(percentage_vaccinated, '^0.[0-9.]+$')
+		WHEN 1 THEN percentage_vaccinated
+		ELSE 5
+	END DESC
+
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+-- Additional queries regarding vaccination numbers: 
 
 
 -- 1.
@@ -84,7 +234,6 @@ JOIN PortfolioProject.CovidVaccinations cv
 WHERE cd.continent != ''
 -- AND cd.location LIKE 'Northern%'
 GROUP BY cd.continent, cd.location, cd.population
-
 
 
 -- 2.
